93248a9da520de20857853a978f01f843c8f903c
Borucinski, Michal <mborucinski@kpmg.pl>
2019-05-12 15:03:23 +0200

    unit test project added plus some small unit tests, resharper setting to put using inside of namspace.

---
+++ test/Equipment/Equipment.UnitTests/EquipmentAggregateTest.cs
@@ -0,0 +1,49 @@
+using System;
+using Xunit;
+
+namespace Boruc.LabEquip.Equipment.UnitTests
+{
+	using Services.Equipment.Domain.AggregatesModel.EquipmentAggregate;
+
+	public class EquipmentAggregateTest
+	{
+		[Fact]
+		public void CreateEquipmentItem_Success()
+		{
+			var name = "equipmentName";
+			var number = "1230980123-123-3123";
+
+			var equipmentItem = new Equipment(name, number);
+
+			Assert.NotNull(equipmentItem);
+		}
+
+		[Fact]
+		public void CreateEquipmentItem_EmptyName_Fail()
+		{
+			var name = string.Empty;
+			var number = "12387_123";
+
+			Assert.Throws<ArgumentNullException>(() => new Equipment(name, number));
+		}
+
+		[Fact]
+		public void CreateEquipmentItem_EmptyNumber_Fail()
+		{
+			var name = "fakeName";
+			var number = string.Empty;
+
+			Assert.Throws<ArgumentNullException>(() => new Equipment(name, number));
+		}
+
+		[Fact]
+		public void CreateBookItem_Success()
+		{
+			var bookId = "123";
+
+			var bookItem = new Book(bookId);
+
+			Assert.NotNull(bookItem);
+		}
+	}
+}
