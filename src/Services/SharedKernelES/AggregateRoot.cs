af5c9fbc1a9eba2aa90e37ceebe9fe638ef53210
Borucinski, Michal <mborucinski@kpmg.pl>
2019-07-01 01:05:45 +0200

    New SharedKernel project for Event Sourcing

---
+++ src/Services/SharedKernelES/AggregateRoot.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SharedKernelES
+{
+	public abstract class AggregateRoot : IAggregateRoot
+	{
+		private readonly ICollection<Event> _events = new List<Event>();
+
+		public abstract Guid Id { get; }
+		public int Version { get; internal set; }
+
+		void IAggregateRoot.ApplyChanges(Event @event)
+		{
+			ApplyChanges(@event);
+		}
+
+		public ICollection<Event> GetPendingEvents()
+		{
+			return _events;
+		}
+
+		public void ClearPendingEvents()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void LoadFromHistory(IEnumerable<Event> history)
+		{
+			foreach (var @event in history)
+			{
+				ApplyChange(@event, false);
+			}
+		}
+
+		protected void ApplyChanges(Event @event)
+		{
+			ApplyChange(@event, true);
+		}
+
+		private void ApplyChange(Event @event, bool b)
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	public class Event { }
+}
---
+++ src/Services/SharedKernelES/IAggregateRoot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SharedKernelES
+{
+	interface IAggregateRoot
+	{
+		int Version { get; }
+		Guid Id { get; }
+		void ApplyChanges(Event @event);
+		ICollection<Event> GetPendingEvents();
+		void ClearPendingEvents();
+	}
+}
