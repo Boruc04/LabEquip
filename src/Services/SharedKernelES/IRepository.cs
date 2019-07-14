4951250f60fc3552c69d6279358d9066714a680c
Borucinski, Michal <mborucinski@kpmg.pl>
2019-07-14 17:27:13 +0200

    Moving methods from IEquipmentRepository to IRepository

--- src/Services/SharedKernelES/IRepository.cs
+++ src/Services/SharedKernelES/IRepository.cs
@@ -2,8 +2,10 @@
 
 namespace Boruc.LabEquip.Services.SharedKernelES
 {
-	public interface IRepository<out TAggregateRoot> where TAggregateRoot : AggregateRoot
+	public interface IRepository<TAggregateRoot> where TAggregateRoot : AggregateRoot
 	{
 		IUnitOfWork UnitOfWork { get; }
+		void Add(TAggregateRoot aggregateRoot, int expectedVersion);
+		TAggregateRoot GetById(Guid id);
 	}	
 }
